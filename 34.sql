c8b110c955bf50ecd17953b124296ddadd32efa4
jansel27 <98138950+jansel27@users.noreply.github.com>
2022-02-11 00:52:10 -0600

    Add files via upload

---
+++ 34.sql
@@ -0,0 +1,51 @@
+select + from bd_facts,tbl_asesores:
+select + from bd_facts.tbl_clientes;
+select + from bd_facts, tbl_facturas;
+
+select
+ase.idAsesor,
+ase.numeroID,
+ase.nombres,
+ase.apellido,
+cl.nombrecompleto,
+cl.numeroID
+from tbl_asesores ase left join tbl_clientes el 00ase.íGAsesor =cl. ídAsesor
+where cl.IdAsesor is mull,
+
+
+select
+cl.idCLiente,
+cl.nombrecompleto,
+F.idFactura,
+F.fechaDmision,
+F.fechavencimiento,
+F.fechapagada
+from tbl_facturas F inner joing tbl_clientes cl on p.idclientec = cl,indlicente 
+where F.fechaVencimiento between '2022-02-01 22;36,30' and '2022-02-10 04:11:10' ;
+
+set @v_idAiesor =0,
+select fr
+
+insert into bd_facts.tbl_asesores(
+cantclientes 
+) values (count())
+
+
+
+
+
+set @v_idCliente =0,
+set @v_numeroID =0,
+set @v_numeroRTN =0,
+set @v_nombrecompleto = ' ';
+set @_edad =0;
+set @v_fechadenacimiento = ' ';
+set @v_direccion = ' ' ;
+set @v_fechaingreso = ' ' ;
+set @v_numeroTelefono = ' ';
+set @v_idTSpoCLiente =1;
+set @v_idCiudad =1;
+set @v_idAsesor =2;
+
+insert into bd_facts.tbl_clientes (
+idCliente,
